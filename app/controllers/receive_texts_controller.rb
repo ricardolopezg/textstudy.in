f30fdcd1037bc4fd39d53fcd3a1fb4148936eff0
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-26 12:10:52 -0400

    experiment response to twilio outbound method to remove xml wrapper

--- app/controllers/receive_texts_controller.rb
+++ app/controllers/receive_texts_controller.rb
@@ -62,27 +62,27 @@ class ReceiveTextsController < ApplicationController
     
       if body == correct_answer && "1" || body == correct_answer && "2" || body == correct_answer && "3" || body == correct_answer && "4"
         if user_response.nil?
-          :body => "Correct.")
+          :body => "Correct."
           end
-          Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Correct")
+          Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Correct"
         else 
-          :body => "You can only respond once per question.")
+          :body => "You can only respond once per question."
           end
         end
       elsif body == "1" || body == "2" || body == "3" || body == "4"
         if user_response.nil?
-          :body => explanation)
+          :body => explanation
           end
           Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Wrong")
         else 
-          :body => "You can only respond once per question.")
+          :body => "You can only respond once per question."
           end
         end
       else
-        :body => "Please send only the number of your response. For Example, type 1 for choice 1.")
+        :body => "Please send only the number of your response. For Example, type 1 for choice 1."
         end
       end
-    end
+    )
   end
 
       
