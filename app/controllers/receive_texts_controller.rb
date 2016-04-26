35aa233348c983396bfaa59d0863fe93bdd578b3
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-26 12:13:43 -0400

    couldn't fix, reverting back to original response xml code

--- app/controllers/receive_texts_controller.rb
+++ app/controllers/receive_texts_controller.rb
@@ -10,83 +10,43 @@ class ReceiveTextsController < ApplicationController
   end
 
 
-  # def response_message
-  #   from = params[:From]
-  #   body = params[:Body]
-
-  #   correct_answer = Question.where(body: Question.where(id: Response.where(to_number: from).last.question_id).first.body).first.correct_answer
-
-  #   explanation = Question.where(body: Question.where(id: Response.where(to_number: from).last.question_id).first.body).first.explanation
-
-  #   user_response = Response.where(to_number: from).last.user_response
-
-  #   # user_last_received_question = Question.where(id: Response.where(to_number: from).last.question_id).first.body
-
-  #   @twiml = Twilio::TwiML::Response.new do |r|
-  #     if body == correct_answer && "1" || body == correct_answer && "2" || body == correct_answer && "3" || body == correct_answer && "4"
-  #         if user_response.nil?
-  #           r.Message "Correct."
-  #           Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Correct")
-  #         else 
-  #           r.Message "You can only respond once per question."
-  #         end
-  #     elsif body == "1" || body == "2" || body == "3" || body == "4"
-  #         if user_response.nil?
-  #           r.Message explanation
-  #           Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Wrong")
-  #         else 
-  #           r.Message "You can only respond once per question."
-  #         end
-  #     else
-  #         r.Message "Please send only the number of your response. For Example, type 1 for choice 1."   
-  #     end
-  #   end
-
-  #   # render makes response happen
-  #   render 'response_message.xml.erb', :content_type => 'text/xml'
-  #   # render 'response_message.xml.erb', :content_type => 'text/html'
-  #   # render 'response_message.xml.erb', :content_type => 'text/plain'
-  # end
-
-
-
-
   def response_message
     from = params[:From]
     body = params[:Body]
 
+    correct_answer = Question.where(body: Question.where(id: Response.where(to_number: from).last.question_id).first.body).first.correct_answer
 
-    @@twilio_client.account.messages.create(
-      :from => @@twilio_phone_number,
-      :to => from,
-    
+    explanation = Question.where(body: Question.where(id: Response.where(to_number: from).last.question_id).first.body).first.explanation
+
+    user_response = Response.where(to_number: from).last.user_response
+
+    # user_last_received_question = Question.where(id: Response.where(to_number: from).last.question_id).first.body
+
+    @twiml = Twilio::TwiML::Response.new do |r|
       if body == correct_answer && "1" || body == correct_answer && "2" || body == correct_answer && "3" || body == correct_answer && "4"
-        if user_response.nil?
-          :body => "Correct."
+          if user_response.nil?
+            r.Message "Correct."
+            Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Correct")
+          else 
+            r.Message "You can only respond once per question."
           end
-          Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Correct"
-        else 
-          :body => "You can only respond once per question."
-          end
-        end
       elsif body == "1" || body == "2" || body == "3" || body == "4"
-        if user_response.nil?
-          :body => explanation
+          if user_response.nil?
+            r.Message explanation
+            Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Wrong")
+          else 
+            r.Message "You can only respond once per question."
           end
-          Response.where(to_number: from).last.update_attributes(user_response: body, response_status: "Wrong")
-        else 
-          :body => "You can only respond once per question."
-          end
-        end
       else
-        :body => "Please send only the number of your response. For Example, type 1 for choice 1."
-        end
+          r.Message "Please send only the number of your response. For Example, type 1 for choice 1."   
       end
-    )
+    end
+
+    # render makes response happen
+    render 'response_message.xml.erb', :content_type => 'text/xml'
+    # render 'response_message.xml.erb', :content_type => 'text/html'
+    # render 'response_message.xml.erb', :content_type => 'text/plain'
   end
-
-      
-
 
 
    
