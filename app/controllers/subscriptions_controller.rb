5b17d3867e38151bd58fd56223187d04697e1e3d
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-05-04 13:50:32 -0400

    active and inactive conflict user message alert

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,16 +2,16 @@ class SubscriptionsController < ApplicationController
 
   def active
     @subscription = Subscription.find(params[:id])
-    
-    if @subscription.active == true
-      @subscription.update_attributes(active: false)
-      @active_status = "Deactivate"
-      @other_status = "Activate"
-    else
-      @subscription.update_attributes(active: true)
-      @active_status = "Activate"
-      @other_status = "Deactivate"
-    end
+    @subscription.active ? @subscription.update(active: false) : @subscription.update(active: true)
+    # if @subscription.active == true
+    #   @subscription.update_attributes(active: false)
+    #   @active_status = "Deactivate"
+    #   @other_status = "Activate"
+    # else
+    #   @subscription.update_attributes(active: true)
+    #   @active_status = "Activate"
+    #   @other_status = "Deactivate"
+    # end
   end
 
 
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,16 +1,25 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
-  # validate :one_active_subscription
+  validate :check_activations, on: :update
+  # validates_uniqueness_of :active, if: :active,  conditions: -> { where.(user_id: current_user.id) }
+  # validates_uniqueness_of :active, if: :active
+  # validates :active, uniqueness: true, if: :active
+
+  def check_activations
+    if Subscription.where(active: true, user_id: user_id).any? and active
+      errors.add(:active, "Deactive other subscription first")
+    end
+  end  
+
+
+
+
+
 
   twilio_sid = ENV["TWILIO_ACCOUNT_SID_1"]
   twilio_token = ENV["TWILIO_AUTH_TOKEN_1"]
   @@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
-  def one_active_subscription
-    if expiration_date.present? && expiration_date < Date.today
-      errors.add(:expiration_date, "can't be in the past")
-    end
-  end
 
   def list_message_data
     @@twilio_client.account.messages.list.each do |m|
