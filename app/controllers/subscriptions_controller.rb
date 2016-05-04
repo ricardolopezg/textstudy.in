55e76e956fd8beb33e63bbcee47efc348bbd9c9b
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-05-04 16:33:01 -0400

    sweetalert added to subject selection

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,6 +3,8 @@ class SubscriptionsController < ApplicationController
   def active
     @subscription = Subscription.find(params[:id])
     @subscription.active ? @subscription.update(active: false) : @subscription.update(active: true)
+    # @active_subscription_subject = Subject.find(Subscription.where(user_id: 47, active: true).first.subject_id).name
+    # @active_subscription_subsection = Subject.find(Subscription.where(user_id: 47, active: true).first.subject_id).subsection
     # if @subscription.active == true
     #   @subscription.update_attributes(active: false)
     #   @active_status = "Deactivate"
